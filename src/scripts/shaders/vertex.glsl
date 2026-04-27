88c4030d632cf026f61c56817efb2d99796a8db0
iwasaki <poettype73@gmail.com>
2026-04-27 09:32:59 +0900

    Fix

--- src/scripts/shaders/vertex.glsl
+++ src/scripts/shaders/vertex.glsl
@@ -12,10 +12,5 @@ void main() {
   vUv = uv;
   vTexIndex = aTexIndex;
 
-  vec3 pos = position;
-
-  float isActive = step(0.1, 1.0 - abs(aTexIndex - uActiveTex));
-  pos += normal * isActive * uProgress * 0.15;
-
   gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(position, 1.0);
 }
